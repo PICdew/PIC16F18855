83f786efb44126af75c327189caa8d58b79b0fb4
Shishir Dey <shishirdey241097@gmail.com>
2018-07-23 08:35:26 +0530

    1.1

---
+++ LED/main.c
@@ -0,0 +1,26 @@
+#include <xc.h>
+
+#define HFINTOSC 6
+
+void delay() {
+    for (int i = 0; i < 20000; i++) {}
+}
+
+void main(void) {
+    
+    /* 1. Select high frequency internal oscillator(HFINTOSC) as the source */
+    OSCCON2 |= (HFINTOSC << 4);
+    
+    /* 2. Configure pin RB0 as output(default) */
+    TRISB &= ~(1 << 0);
+    
+    /* 3. Toggle the voltage on pin RB0 repeatedly */
+    while (1) {
+        LATB |= (1 << 0);
+        delay();
+        LATB &= ~(1 << 0);
+        delay();
+    }
+    
+    return;
+}
